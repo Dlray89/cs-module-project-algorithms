6d56121448fa73e43a70e6b6065642d4b239b08a
DAP <dlrayjr89@gmail.com>
2020-07-22 19:45:23 +0000

    single number is done

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,12 +3,10 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-   single_num = 0
-
-   for i in arr:
-       single_num ^= i
-   return single_num
+    num = 0
+    for i in arr:
+        num ^= i
+    return num
 
 
 if __name__ == '__main__':
